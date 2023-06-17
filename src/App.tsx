6db6142e3b3588477119b93573738d801ac8127d
jirimicvl <jirimicvl@outlook.com>
2023-06-17 18:57:49 +0300

    Added portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Hero from './component/Hero'
+import Portfolio from './component/Portfolio';
 
 function App() {
   return (
     <>
       <Hero />
+      <Portfolio />
       <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
       <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
       <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
