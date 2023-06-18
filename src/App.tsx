294e6f246ba0636c116909559cca7d60416b3c5f
jirimicvl <jirimicvl@outlook.com>
2023-06-18 19:02:38 +0300

    Added profile, team, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 
 import './App.css';
+import Footer from './component/Footer';
 import Hero from './component/Hero'
 import Portfolio from './component/Portfolio';
+import Profile from './component/Profile';
+import Team from './component/Team';
 
 function App() {
   return (
     <>
       <Hero />
+      <Profile />
       <Portfolio />
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis nihil expedita atque harum obcaecati fugit laboriosam possimus saepe quos facere.</div>
+      <Team />
+      <Footer />
     </>
   );
 }
