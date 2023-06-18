4dce1ece0fdecc0f1b04e197e980d396027c0316
jirimicvl <jirimicvl@outlook.com>
2023-06-18 19:02:15 +0300

    Added Portfolio section

---
+++ src/component/Profile.tsx
@@ -0,0 +1,37 @@
+export default function Profile() {
+    return (
+        <section style={{ backgroundColor: '#00dcda' }}>
+            <div className="px-6 py-20 sm:px-6 sm:py-24 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-4xl font-bold tracking-tight sm:text-4xl"  
+                    style={{ color: 'var(--ext-dark-blue-01)' }}
+                >
+                    Ready to dive in?
+                </h2>
+                <p className="font-semibold mx-auto mt-6 max-w-xl text-2xl leading-8 text-white">
+                    Hi there ! I am Jere Sulovuo. An aspiring V-shaped programmer.
+                </p>
+                {/* <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <a
+                        href="#"
+                        className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        style={{ backgroundColor: 'var(--ext-grey)', color: 'var(--ext-dark-blue-01)' }}
+                    >
+                    Portfolio
+                    </a>
+                    <a
+                        href="#"
+                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                    Get started
+                    </a>
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                    Learn more <span aria-hidden="true">→</span>
+                    </a>
+                </div> */}
+                </div>
+            </div>
+        </section>
+    )
+  }
+  
