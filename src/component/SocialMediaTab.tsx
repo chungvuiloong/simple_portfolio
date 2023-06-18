acc32b1c7b1283304c9b0295e298507308fdb11d
jirimicvl <jirimicvl@outlook.com>
2023-06-18 08:21:57 +0300

    Added tailwind

--- src/component/SocialMediaTab.tsx
+++ src/component/SocialMediaTab.tsx
@@ -6,10 +6,11 @@ interface SocialMediaTabProps {
   icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 }
 
-const SocialMediaTab: React.FC<SocialMediaTabProps> = ({ href, icon: Icon }) => {
+const SocialMediaTab: React.FC<SocialMediaTabProps> = ({ name, href, icon: Icon }) => {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="social-tab">
-      <Icon className="social-tab__icon" />
+    <a href={href} target="_blank" rel="noopener noreferrer" className="flex items-center text-black no-underline mr-2">
+      <Icon className="w-5 h-5 mr-1" />
+      {/* <span className="text-sm">{name}</span> */}
     </a>
   );
 };
