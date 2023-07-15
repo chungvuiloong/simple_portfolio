81241ce67cb87749d7a9fd25f45869f954638d7c
jirimicvl <jirimicvl@outlook.com>
2023-07-15 13:24:41 +0300

    Added animation

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -1,35 +1,54 @@
+import { useEffect, useState } from "react";
+
+const FADE_INTERVAL_MS = 1500;
+const WORD_CHANGE_INTERVAL_MS = FADE_INTERVAL_MS * 2;
+const HASH_TAGS_WORDS = [
+    `UX/ UI`,
+    `Service Design`,
+    `Front-end`,
+    `Back-end`,
+    `Cloud`,
+];
+
+type FadeProp = { fade: 'fade-in' | 'fade-out' };
+
 export default function Profile() {
+    const [fadeProp, setFadeProp] = useState<FadeProp>({ fade: 'fade-in' });
+    const [wordOrder, setWordOrder] = useState(0);
+
+    useEffect(() => {
+        const fadeTimeout = setInterval(() => {
+          setFadeProp((prevFadeProp) => ({
+            fade: prevFadeProp.fade === 'fade-in' ? 'fade-out' : 'fade-in'
+          }));
+        }, FADE_INTERVAL_MS);
+    
+        return () => clearInterval(fadeTimeout);
+      }, [fadeProp]);
+    
+      useEffect(() => {
+        const wordTimeout = setInterval(() => {
+          setWordOrder((prevWordOrder) => (prevWordOrder + 1) % HASH_TAGS_WORDS.length);
+        }, WORD_CHANGE_INTERVAL_MS);
+    
+        return () => clearInterval(wordTimeout);
+      }, []);
+      
     return (
         <section style={{ backgroundColor: '#00dcda' }}>
-            <div className="px-6 py-20 sm:px-6 sm:py-24 lg:px-8">
-                <div className="mx-auto max-w-2xl text-center">
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
                 <h2 className="text-4xl font-bold tracking-tight sm:text-4xl"  
                     style={{ color: 'var(--ext-dark-blue-01)' }}
                 >
                     Ready to dive in?
                 </h2>
                 <p className="font-semibold mx-auto mt-6 max-w-xl text-2xl leading-8 text-white">
-                    Hi there ! I am Jere Sulovuo. An aspiring V-shaped programmer.
+                    Hi there ! I am Jere Sulovuo. <br />
+                    An aspiring V-shaped programmer who has strong passion in&nbsp; 
+                    <span className={fadeProp.fade} style={{ color: 'var(--ext-dark-blue-01)' }}>
+                        {HASH_TAGS_WORDS[wordOrder]}
+                    </span>
                 </p>
-                {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <a
-                        href="#"
-                        className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        style={{ backgroundColor: 'var(--ext-grey)', color: 'var(--ext-dark-blue-01)' }}
-                    >
-                    Portfolio
-                    </a>
-                    <a
-                        href="#"
-                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                    Get started
-                    </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                    Learn more <span aria-hidden="true">→</span>
-                    </a>
-                </div> */}
-                </div>
             </div>
         </section>
     )
