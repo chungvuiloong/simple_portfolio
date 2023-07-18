2cbe8c9f33feaea5164eaf4455f58bd2fa14a8a0
jirimicvl <jirimicvl@outlook.com>
2023-07-18 10:23:48 +0300

    EDitted padding

--- src/component/Portfolio.tsx
+++ src/component/Portfolio.tsx
@@ -110,7 +110,7 @@ interface PortfolioTabProps {
     };
   
     return (
-      <section className="px-6 py-24 sm:py-32 lg:px-8" style={{ backgroundColor: '#00dcda' }} >
+      <section className="px-6 py-12 sm:py-8 lg:px-8" style={{ backgroundColor: '#00dcda' }} >
         <div className="mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between">
           <h2 className="text-4xl font-bold tracking-tight sm:text-4xl"  
                 style={{ color: 'var(--ext-dark-blue-01)' }}
