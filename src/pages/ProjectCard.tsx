df1a9c586de306636e468b1790a50f39517dc62d
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-10 21:31:46 +0300

    FIxed card size.

--- src/pages/ProjectCard.tsx
+++ src/pages/ProjectCard.tsx
@@ -25,23 +25,24 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     const { title, description, imageUrl, demoUrl, githubUrl } = project;
     return (
     <Card className='
-    sm:
         basis-1/4
         pl-5 py-3
+
         border-2 border-blue-900 rounded-xl
+        min-w-[200px]
         font-san'
     >
-        <Typography variant='h4' className='pb-3 text-end self-end w-full'>
+        <Typography variant='h4' className='basis-1/4 pb-3 text-end self-end w-full'>
             <div className='pr-5'>{title}</div>
             <hr className="h-3 border-t-0 bg-blue-900 rounded-l-lg" />
         </Typography>
-        <CardBody className="pr-5">
+        <CardBody className="basis-1/4 pr-5">
             <Typography variant='paragraph'>
                 {description}
             </Typography>
         </CardBody>
 
-        <CardFooter className="pr-5 flex justify-between">
+        <CardFooter className="basis-1/4  pr-5 flex justify-between">
             {/* <a
                 href={demoUrl}
                 target="_blank"
