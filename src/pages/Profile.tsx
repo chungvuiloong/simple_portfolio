49d81844ded7b424bad00acc19b2a85ceb66a334
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-28 10:44:10 +0300

    Updated container

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -34,24 +34,26 @@ export default function Profile() {
         return () => clearInterval(wordTimeout);
       }, []);
 
-    return (
-        <section className="px-6 py-24 sm:py-32 lg:px-8" style={{ backgroundColor: '#00dcda' }}>
-            <div className="mx-auto max-w-7xl  lg:flex lg:items-center lg:justify-between">
-                <h2 className="text-4xl tracking-tight sm:text-4xl 
-                font-sans font-600
-                "  
-                    style={{ color: 'var(--ext-dark-blue-01)' }}
-                >
-                    Ready to dive in?
-                </h2>
-                <p className="font-semibold mt-6 text-2xl leading-8 text-white text-end">
-                    Hi there ! I am Jere Sulovuo<br/>
-                    An aspiring V-shaped programmer<br /> 
-                    who has strong passion in<br /> 
-                    <span className={fadeProp.fade} style={{ color: 'var(--ext-dark-blue-01)' }}>
-                        {INTERESTS[wordOrder]}
-                    </span>
-                </p>
+    return (        
+        <section className='py-24 sm:py-32 px-6 lg:px-10' style={{ backgroundColor: '#00dcda' }} >
+            <div className='mx-auto container'>
+                <div className="lg:flex lg:items-center lg:justify-between">
+                    <h2 className="text-4xl tracking-tight sm:text-4xl 
+                    font-sans font-600
+                    "  
+                        style={{ color: 'var(--ext-dark-blue-01)' }}
+                    >
+                        Ready to dive in?
+                    </h2>
+                    <p className="font-semibold mt-6 text-2xl leading-8 text-white text-end">
+                        Hi there ! I am Jere Sulovuo<br/>
+                        An aspiring V-shaped programmer<br /> 
+                        who has strong passion in<br /> 
+                        <span className={fadeProp.fade} style={{ color: 'var(--ext-dark-blue-01)' }}>
+                            {INTERESTS[wordOrder]}
+                        </span>
+                    </p>
+                </div>
             </div>
         </section>
     )
