410b547e51c83cbdb86f372555620efb89320f76
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-21 14:28:58 +0300

    Added sun and moon image.

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,4 +1,37 @@
+import { SVGProps } from 'react';
 import useLocalStorage from 'use-local-storage'
+
+function SunIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
+      <path
+        d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
+        fill="none"
+      />
+    </svg>
+  )
+}
+
+function MoonIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
 
 export default function Home() {
     const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -8,13 +41,20 @@ export default function Home() {
         const newTheme = theme === 'light' ? 'dark' : 'light';
         setTheme(newTheme);
       }
+      console.log(theme);
     
       return (
-        <div className="app" data-theme={theme}>
-          <span>Easy Darkmode and Themes in React</span>
-          <button onClick={switchTheme}>
-            Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
-          </button>
+        <div data-theme={theme} className='text-white self-center'>
+            <button
+            type="button"
+            className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+            onClick={switchTheme}
+            >
+                { theme === 'light' ? 
+                    <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition" />
+                    : <MoonIcon className="h-6 w-6 fill-zinc-700 transition dark:block " />
+                }
+             </button>
         </div>
       );
 }
