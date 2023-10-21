a23a860b2a91856df38940b1a01610320ed55fd1
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-21 22:59:32 +0300

    Deleted unused code.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,14 @@
 import './style.css'
 import React, { useState, useEffect } from 'react';
-// import dark_sky from '../assets/images/darkSky.png'
-// import stars_sky from '../assets/images/stars.png'
-// import left_meteor from '../assets/images/meteorLeft.png'
-// import white_moon from '../assets/images/whiteMoonBckRing.png'
-// import back_mountains from '../assets/images/backMountains.png'
-// import sea from '../assets/images/seaNoMountains.png'
-// import front_island from '../assets/images/frontIsland.png'
-// import bottom_cloud from '../assets/images/bottomCloud.png'
-// import upper_cloud from '../assets/images/upperCloud.png'
+import dark_sky from '../assets/images/darkSky.png'
+import stars_sky from '../assets/images/stars.png'
+import left_meteor from '../assets/images/meteorLeft.png'
+import white_moon from '../assets/images/whiteMoonBckRing.png'
+import back_mountains_night from '../assets/images/backMountains.png'
+import sea from '../assets/images/seaNoMountains.png'
+import front_island from '../assets/images/frontIsland.png'
+import bottom_cloud_night from '../assets/images/bottomCloud.png'
+import upper_cloud_night from '../assets/images/upperCloud.png'
 
 
 import sunny_sky from '../assets/images/sunny_sky.png'
@@ -20,15 +20,137 @@ import bottom_cloud from '../assets/images/bottomCloud_day.png'
 import upper_cloud from '../assets/images/upperCloud_day.png'
 import cloudy from '../assets/images/cloudy.png'
 import DarkModeButton from '../components/DarkModeButton'
+import useLocalStorage from 'use-local-storage'
+import { defaultTheme } from '../helpers/theme';
+
+
+
 
 export default function Hero() {
     const [scrollY, setScrollY] = useState<number>(0);
+    const [theme] = useLocalStorage('theme', defaultTheme ? 'dark' : 'light');
 
     useEffect(() => {
       const handleScroll = () => setScrollY(window.scrollY);
       window.addEventListener('scroll', handleScroll);
       return () => window.removeEventListener('scroll', handleScroll);
     });
+
+    const SunnyDay = () => {
+        return (
+            <>
+                <img
+                    src={`${sunny_sky}`}
+                    id="darkSky"
+                    alt="The Beautiful Dark Sky"
+                    className="layer"
+                />
+                <img
+                    src={cloudy}
+                    id="stars"
+                    alt="starts in sky"
+                    className="stars blend layer"
+                    style={{ bottom: `${(scrollY * 0.50)}px`}}
+                />
+                <img
+                    src={sun}
+                    id="whiteMoon"
+                    alt="moon"
+                    className="whiteMoon blend blur-[10px]"
+                    style={{ top: `${(scrollY * 1.25)}px`} }
+                />
+                <img
+                    src={upper_cloud}
+                    id="upperCloud"
+                    alt="cloud"
+                    className="layer upperCloud blend"
+                />            
+                <img
+                    src={bottom_cloud}
+                    id="bottomCloud"
+                    alt="cloud"
+                    className="layer bottomCloud blend"
+                    style={{ top: `${scrollY * 0.25}px` } }
+                />
+                <img
+                    src={back_mountains}
+                    id="backMountains"
+                    alt="mountain"
+                    className="layer"
+                    style={{ top: `${scrollY * 0.1}px` } }
+                />
+                <img src={sea_day} id="sea" className="layer" alt="sea"/>
+                <img
+                    src={front_island_day}
+                    id="frontIsland"
+                    alt="island"
+                    className="layer frontIsland"
+                />
+            </>
+        )
+    }
+
+    const Night = () => {
+        return (
+            <>
+                <img
+                    src={dark_sky}
+                    id="darkSky"
+                    alt="The Beautiful Dark Sky"
+                    className="layer"
+                />
+                <img
+                    src={stars_sky}
+                    id="stars"
+                    alt="starts in sky"
+                    className="stars blend layer"
+                    style={{ top: `${(scrollY * 0.25)}px`, left: `${(scrollY * 0.05)}px`}}
+                />
+                <img
+                    src={left_meteor}
+                    id="meteor"
+                    alt="meteor"
+                    className="meteor"
+                    style={{ top: `${scrollY}px`, left: `${scrollY}px` } }
+                />
+                <img
+                    src={white_moon}
+                    id="whiteMoon"
+                    alt="moon"
+                    className="whiteMoon blend"
+                    style={{ top: `${(scrollY * 1.25)}px`} }
+                />
+            
+                <img
+                    src={bottom_cloud_night}
+                    id="bottomCloud"
+                    alt="cloud"
+                    className="layer bottomCloud blend"
+                    style={{ top: `${scrollY * 0.25}px` } }
+                />
+                <img
+                    src={upper_cloud_night}
+                    id="upperCloud"
+                    alt="cloud"
+                    className="layer upperCloud blend"
+                />            
+                <img
+                    src={back_mountains_night}
+                    id="backMountains"
+                    alt="mountain"
+                    className="layer"
+                    style={{ top: `${scrollY * 0.1}px` } }
+                />   
+                <img src={sea} id="sea" className="layer" alt="sea"/>            
+                <img
+                    src={front_island}
+                    id="frontIsland"
+                    alt="island"
+                    className="layer frontIsland"
+                />
+            </>
+        )
+    }
 
   return (
     <div className='bg-[#8f81fc] dark:bg-black'>  
@@ -41,111 +163,8 @@ export default function Hero() {
         </div>
 
         <section className="layer-parent" id="heroBanner">
-            {/* <h2 className="empty">Empty</h2> */}
-            {/* <img
-                src={dark_sky}
-                id="darkSky"
-                alt="The Beautiful Dark Sky"
-                className="layer"
-            />
-            <img
-                src={stars_sky}
-                id="stars"
-                alt="starts in sky"
-                className="stars blend layer"
-                style={{ top: `${(scrollY * 0.25)}px`, left: `${(scrollY * 0.05)}px`}}
-            />
-            <img
-                src={left_meteor}
-                id="meteor"
-                alt="meteor"
-                className="meteor"
-                style={{ top: `${scrollY}px`, left: `${scrollY}px` } }
-            />
-            <img
-                src={white_moon}
-                id="whiteMoon"
-                alt="moon"
-                className="whiteMoon blend"
-                style={{ top: `${(scrollY * 1.25)}px`} }
-            />
-         
-            <img
-                src={bottom_cloud}
-                id="bottomCloud"
-                alt="cloud"
-                className="layer bottomCloud blend"
-                style={{ top: `${scrollY * 0.25}px` } }
-            />
-            <img
-                src={upper_cloud}
-                id="upperCloud"
-                alt="cloud"
-                className="layer upperCloud blend"
-            />            
-            <img
-                src={back_mountains}
-                id="backMountains"
-                alt="mountain"
-                className="layer"
-                style={{ top: `${scrollY * 0.1}px` } }
-            />   
-            <img src={sea} id="sea" className="layer" alt="sea"/>            
-            <img
-                src={front_island}
-                id="frontIsland"
-                alt="island"
-                className="layer frontIsland"
-            /> */}
-
-
-            <img
-                src={sunny_sky}
-                id="darkSky"
-                alt="The Beautiful Dark Sky"
-                className="layer"
-            />
-            <img
-                src={cloudy}
-                id="stars"
-                alt="starts in sky"
-                className="stars blend layer"
-                style={{ bottom: `${(scrollY * 0.50)}px`}}
-            />
-            <img
-                src={sun}
-                id="whiteMoon"
-                alt="moon"
-                className="whiteMoon blend blur-[10px]"
-                style={{ top: `${(scrollY * 1.25)}px`} }
-            />
-            <img
-                src={upper_cloud}
-                id="upperCloud"
-                alt="cloud"
-                className="layer upperCloud blend"
-            />            
-            <img
-                src={bottom_cloud}
-                id="bottomCloud"
-                alt="cloud"
-                className="layer bottomCloud blend"
-                style={{ top: `${scrollY * 0.25}px` } }
-            />
-            <img
-                src={back_mountains}
-                id="backMountains"
-                alt="mountain"
-                className="layer"
-                style={{ top: `${scrollY * 0.1}px` } }
-            />
-            <img src={sea_day} id="sea" className="layer" alt="sea"/>
-            <img
-                src={front_island_day}
-                id="frontIsland"
-                alt="island"
-                className="layer frontIsland"
-            />
+        {/* <Night /> */}
+        {/* <SunnyDay /> */}
       </section>
     </div>
   )
