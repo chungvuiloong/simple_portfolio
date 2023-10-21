11cb3634263039fe3edfdcd92793c81d8a8e2756
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-21 22:34:31 +0300

    Added them and fixed no dark mode toggle issue.

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Hero from './components/Hero'
 import Portfolio from './components/Portfolio';
 import Profile from './components/Profile';
 import Team from './components/Team';
+import useLocalStorage from 'use-local-storage'
+import { defaultTheme } from './helpers/theme';
 
 function App() {
+    const [theme] = useLocalStorage('theme', defaultTheme ? 'dark' : 'light');
+
   return (
-    <>
+    <div className={theme}>
       <Hero />
       <Profile />
       <Portfolio />
       <Team />
       {/* <Footer /> */}
-    </>
+    </div>
   );
 }
 
