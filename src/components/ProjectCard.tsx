68d65378ab0206f90d88139fe180a493e0a3de27
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-22 18:28:30 +0300

    Updated card with DaisyUI

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,13 +1,6 @@
-import {
-    Card,
-    Typography,
-    Button,
-    IconButton,
-    CardBody,
-    CardFooter,
-} from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
 import React, { ReactNode } from 'react';
-import { FaGithub } from 'react-icons/fa';
+import Iconify from "./Iconify";
 
 interface Project {
     title?: string;
@@ -26,72 +19,39 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     const { title, description, imageUrl, demoUrl, githubUrl, status } = project;
    
     return (
-    // <Card className='
-    //     basis-1/4
-    //     pl-5 py-3
-    //     flex flex-col
-    //     border-2 border-blue-900 rounded-xl
-    //     min-w-[200px]
-    //     font-san'
-    // >
-    //     <Typography variant='h4' className='grow'>
-    //         <div className='pr-5'>{title}</div>
-    //     </Typography>
-    //     <CardBody className="grow-0 pr-5">
-    //         {/* <hr className="h-3 border-t-0 bg-blue-900 rounded-l-lg" /> */}
-    //         <Typography variant='paragraph'>
-    //             {description}
-    //         </Typography>
-    //     </CardBody>
-
-    //     {/* <CardFooter className="grow-0 pr-5 flex justify-between">
-            // <a
-            //     href={demoUrl}
-            //     target="_blank"
-            //     rel="noopener noreferrer"
-            //     className="my-auto text-blue-500 hover:text-blue-700"
-            //     >
-            //     See more
-            // </a>
-            // <Button className='flex gap-1 bg-gray-800 w-fit h-fit'>
-            //     <FaGithub className="text-white my-auto" />
-            //     <span>Github</span>
-            // </Button>
-    //         </CardFooter> */}
-    // </Card>
-    <Card className='flex flex-col pl-5 py-3 w-[300px]'>
-        <Typography variant='h4' className='min-h-[70px] flex flex-col-reverse pr-5 text-end'>
-            {title}
-        </Typography>
-        <CardBody className='mt-2' style={{ height: '-webkit-fill-available'}}>
-            <hr className="h-3 border-t-0 bg-blue-900 rounded-l-lg" />
-            <div className="mt-5 pr-5">
-                <Typography >{status}</Typography>
-                <Typography variant='paragraph'>
-                    {description}
-                </Typography>
+        <div className='card bg-Off-white flex flex-col pl-5 py-3 w-[300px]'>
+            <Typography variant='h4' className='min-h-[70px] flex flex-col-reverse pr-5 text-end text-slate-700'>
+                {title}
+            </Typography>
+            <div className='mt-2' style={{ height: '-webkit-fill-available'}}>
+                <hr className="h-3 border-t-0 bg-blue-900 rounded-l-lg" />
+                <div className="mt-5 pr-5">
+                    <Typography className="text-slate-700">{status}</Typography>
+                    <Typography variant='paragraph' className="text-slate-500">
+                        {description}
+                    </Typography>
+                </div>
             </div>
-        </CardBody>
-        <CardFooter className="mt-5 pr-5 flex justify-between">
-            <a
-                href={demoUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="my-auto text-blue-500 hover:text-blue-700"
-                >
-                See more
-            </a>
-            {
-                githubUrl !== undefined ?         
-                    <a href={githubUrl}>
-                        <Button className='flex gap-2 bg-gray-800 w-fit h-fit'>
-                            <FaGithub className="text-white my-auto" />
-                            <Typography variant="small" color="white">Github</Typography>
-                        </Button>
-                    </a>
-                : <></>
-            }
-        </CardFooter>
-    </Card>
-  );
+            <div className="mt-5 pr-5 flex justify-between">
+                <a
+                    href={demoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="my-auto text-blue-500 hover:text-blue-700"
+                    >
+                    See more
+                </a>
+                {
+                    githubUrl !== undefined ?         
+                        <a href={githubUrl}>
+                            <button className="btn btn-neutral flex gap-2 w-fit h-fit">
+                                <Iconify icon="icomoon-free:github" className="text-white" />
+                                <Typography variant="small" color="white">GitHub</Typography>
+                            </button>
+                        </a>
+                    : <></>
+                }
+            </div>
+        </div>
+    );
 };
