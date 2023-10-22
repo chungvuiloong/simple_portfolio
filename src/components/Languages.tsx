6f1d1bc1b5e4f22852d62fd8c2a5d865e285a6b8
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-22 19:45:27 +0300

    Add language component.

---
+++ src/components/Languages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+const Tailwind = () => {
+    return (
+        <div>
+            <Icon icon="logos:tailwindcss" />
+        </div>
+    );
+};
+
+export default Tailwind;
