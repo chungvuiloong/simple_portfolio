73705a413ed86f965122e698f27f98acb3ba12ce
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-23 13:32:21 +0300

    Added react logo.

--- src/components/TechnologiesUsed.tsx
+++ src/components/TechnologiesUsed.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const TechnologiesUsed = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+type TechnologyLogos = { [key: string]: string };
+
+const TechnologiesUsed = ({ array }: { array?: string[] }) => {
+  const technologyLogos: TechnologyLogos = {
+    React: 'devicon:react-wordmark',
+  };
+
+  return (
+    <div>
+      {array?.map((tech, index) => (
+        <span key={index}>
+          {tech}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default TechnologiesUsed;
