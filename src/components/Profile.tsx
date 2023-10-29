6e4199a8d7ca4a515b5b3958e5c692f9dd866542
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-29 21:46:54 +0200

    Updated the career.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 const FADE_INTERVAL_MS = 1500;
 const WORD_CHANGE_INTERVAL_MS = FADE_INTERVAL_MS * 2;
 const INTERESTS = [
+    `Artificial Intelligence (AI)`,
     `UX/ UI`,
     `Service Design`,
-    `Front-end`,
-    `Back-end`,
-    `Cloud`,
+    `Front-end development`,
+    `Back-end develoment`,
+    `Cloud engineering`,
 ];
 
 type FadeProp = { fade: 'fade-in' | 'fade-out' };
@@ -53,6 +54,10 @@ export default function Profile() {
                           <span className={fadeProp.fade} style={{ color: 'var(--ext-dark-blue-01)' }}>
                               {INTERESTS[wordOrder]}
                           </span>
+                      </Typography>
+                      <Typography variant="h4" color="white" className="mt-6 text-left">
+                        Test
+
                       </Typography>
                     </div>
                 </div>
