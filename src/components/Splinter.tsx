641d01b7da0e3c220c0981f738ab88dcbd859b66
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-11-21 16:00:07 +0200

    Changed word to splinter.

---
+++ src/components/Splinter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Splinter = () => {
+    return (
+        <section className='bg-[#00dcda]'>                     
+            <h2 className='tracking-tight xl:text-[20rem] lg:text-[13rem] md:text-[10rem] text-[6rem] break-words
+                font-sans font-500'  
+                style={{ color: 'var(--ext-dark-blue-01)' }}
+            >
+                Splinter
+            </h2>
+            
+        </section>
+    );
+};
+
+export default Splinter;
