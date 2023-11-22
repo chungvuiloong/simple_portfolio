a686300ebd4f1b54c0271fbbf3b847ce96461aec
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-11-22 15:50:52 +0200

    Added new Spline

--- src/components/Splinter.tsx
+++ src/components/Splinter.tsx
@@ -3,14 +3,18 @@ import Spline from '@splinetool/react-spline';
 
 const Splinter = () => {
     return (
-        <section className='bg-[#00dcda]'>                     
-            <h2 className='tracking-tight xl:text-[20rem] lg:text-[13rem] md:text-[10rem] text-[6rem] break-words
-                font-sans font-500'  
-                style={{ color: 'var(--ext-dark-blue-01)' }}
-            >
-                Splinter
-            </h2>
-            <Spline scene="https://prod.spline.design/GLErkvWSW79m5Ro6/scene.splinecode" />
+        <section className='bg-[#00dcda]'>
+            <div className='container'>            
+                <h2 className='tracking-tight xl:text-[20rem] lg:text-[13rem] md:text-[10rem] text-[6rem] break-words
+                    font-sans font-500'  
+                    style={{ color: 'var(--ext-dark-blue-01)' }}
+                >
+                    Splinter
+                </h2>
+                <div className='flex relative h-[100%] w-[100%]'>
+                    <Spline scene="https://prod.spline.design/e5UkyXm-ovTcJuPl/scene.splinecode" />
+                </div>      
+            </div>               
         </section>
     );
 };
