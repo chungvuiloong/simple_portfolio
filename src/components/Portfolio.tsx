770782619d6888dfee84df1f4b811fae2fd9f392
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-12-07 11:53:49 +0200

    Removed tracking-tight

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -305,7 +305,7 @@ interface PortfolioTabProps {
         <section className='py-24 sm:py-32 px-6 lg:px-10' style={{ backgroundColor: '#00dcda' }} >
             <div className='mx-auto container'>
                 <div className='mb-10 flex lg:items-center gap-10'>
-                     <h2 className='tracking-tight xl:text-[20rem] lg:text-[13rem] md:text-[10rem] text-[5rem] break-words
+                     <h2 className='xl:text-[20rem] lg:text-[13rem] md:text-[10rem] text-[5rem] break-words
                         font-sans font-500 md:leading-[16rem] leading-[5rem] text-[#00224c] self-center'
                     >
                     Port<span className='sm:hidden'><br /></span>folio
