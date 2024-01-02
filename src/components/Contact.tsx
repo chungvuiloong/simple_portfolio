d81f0d18096c3bd41604d42b3c3f55ddd1985e17
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-01-02 15:49:42 +0200

    Added paragraph content

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,14 +4,16 @@ const Contact = () => {
     return (
         <section className='py-24 sm:py-32 px-6 lg:px-10' style={{ backgroundColor: '#00dcda' }} >
             <div className='mx-auto container'>
-                <div className='mb-10 flex lg:items-center gap-10'>
+                <div className='flex flex-col flex-start'>
                      <h2 className='xl:text-[16rem] lg:text-[13rem] md:text-[10rem] text-[5rem] break-words
-                        font-sans font-500 md:leading-[16rem] leading-[5rem] text-[#00224c] self-center tracking-tighter'
+                        font-sans font-500 md:leading-[16rem] leading-[5rem] text-[#00224c] tracking-tighter'
                     >
                         Reach Out
                     </h2>
-                    
-
+                    <p className='mt-6 text-left md:text-4xl text-md text-black opacity-50'>
+                        New opportunities? projects? components? pages? Tell me more.
+                        Connect with me via email at chungvuiloong@gmail.com
+                    </p>
                 </div>
             </div>
         </section>
